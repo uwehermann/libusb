c6921b03461515b08a652e7e670148201151747a
Chris Dickens <christopher.a.dickens@gmail.com>
2015-04-08 00:40:48 -0700

    Windows: Implement event handling functionality
    This change makes the Windows version of the event handling abstraction
    functional. Event handling is implemented using WaitForMultipleObjects().
    
    Signed-off-by: Chris Dickens <christopher.a.dickens@gmail.com>

--- libusb/os/events_windows.c
+++ libusb/os/events_windows.c
@@ -20,6 +20,8 @@
 
 #include <config.h>
 
+#include <assert.h>
+
 #include "libusbi.h"
 
 int usbi_create_event(usbi_event_t *event)
@@ -140,11 +142,78 @@ int usbi_destroy_timer(usbi_timer_t timer)
 
 int usbi_alloc_event_data(struct libusb_context *ctx)
 {
-	return LIBUSB_ERROR_OTHER;
+	struct usbi_event_source *event_source;
+	HANDLE *handles = (HANDLE *)ctx->event_data;
+	int i = 0;
+
+	/* Windows is fundamentally different from other platforms in that it imposes a
+	 * rather small limit on the number of HANDLEs you can wait for. Since the Windows
+	 * backends will alter the event source list for every submitted transfer, we can
+	 * optimize a little by allocating the largest array of HANDLEs supported just one
+	 * time. On subsequent calls to this function, we will save time by not re-allocating
+	 * and instead just update the elements in the array. */
+	if (!handles) {
+		handles = calloc(MAXIMUM_WAIT_OBJECTS, sizeof(HANDLE));
+		if (!handles)
+			return LIBUSB_ERROR_NO_MEM;
+		ctx->event_data = handles;
+	}
+
+	list_for_each_entry(event_source, &ctx->event_sources, list, struct usbi_event_source) {
+		if (i == MAXIMUM_WAIT_OBJECTS) {
+			usbi_warn(ctx, "too many HANDLEs to wait on, some will be ignored!");
+			break;
+		}
+		handles[i++] = event_source->pollfd.fd;
+	}
+
+	return 0;
 }
 
 int usbi_handle_events(struct libusb_context *ctx, void *event_data, unsigned int cnt,
 	unsigned int internal_cnt, int timeout_ms)
 {
-	return LIBUSB_ERROR_OTHER;
+	HANDLE *handles = (HANDLE *)event_data;
+	DWORD result;
+	int r;
+
+	/* make sure to wait for at most MAXIMUM_WAIT_OBJECT HANDLEs */
+	cnt = MIN(cnt, MAXIMUM_WAIT_OBJECTS);
+	assert(internal_cnt <= cnt);
+
+	usbi_dbg("WaitForMultipleObjects() for %u HANDLEs with timeout in %dms", cnt, timeout_ms);
+	result = WaitForMultipleObjects((DWORD)cnt, handles, FALSE, (DWORD)timeout_ms);
+	usbi_dbg("WaitForMultipleObjects() returned %d", result);
+	if (result == WAIT_TIMEOUT)
+		return usbi_using_timer(ctx) ? 0 : LIBUSB_ERROR_TIMEOUT;
+	else if (result == WAIT_FAILED) {
+		usbi_err(ctx, "WaitForMultipleObjects() failed err=%d", GetLastError());
+		return LIBUSB_ERROR_IO;
+	}
+
+	result -= WAIT_OBJECT_0;
+
+	/* handles[0] is always the event */
+	if (result == 0) {
+		r = usbi_handle_event_trigger(ctx);
+		if (r < 0) {
+			/* return error code */
+			return r;
+		}
+	}
+
+	/* on timer configurations, handles[1] is the timer */
+	if (usbi_using_timer(ctx) && result == 1) {
+		r = usbi_handle_timer_trigger(ctx);
+		if (r < 0) {
+			/* return error code */
+			return r;
+		}
+	}
+
+	r = usbi_backend->handle_events(ctx, handles + internal_cnt, cnt - internal_cnt, cnt - internal_cnt);
+	if (r)
+		usbi_err(ctx, "backend handle_events failed with error %d", r);
+
+	return r;
 }
